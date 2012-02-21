711748d996e31bdb3061cbee4f264ef873ff07d7
JericoRevote <JericoRevote@02397290-7de5-4d49-84a4-86ba14fd681f>
2012-02-21 03:32:35 +0000

    No need to start VRDeviceDaemon ourselves anymore.
    git-svn-id: https://www.underworldproject.org/svn/LidarViewer@6 02397290-7de5-4d49-84a4-86ba14fd681f

--- Scripts/launchElMayor-2.2-003.sh
+++ Scripts/launchElMayor-2.2-003.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-sudo /sbin/service VRDeviceDaemon-2.2-003 stop
-sudo /sbin/service VRDeviceDaemon-2.2-003 start
-sleep 20
-
 /usr/local/packages/LidarViewer/2.8/bin/LidarViewer points.out -rootSection dante
-
-sudo /sbin/service VRDeviceDaemon-2.2-003 stop
