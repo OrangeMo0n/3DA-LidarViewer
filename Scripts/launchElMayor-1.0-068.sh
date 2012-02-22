23aa85cb8606ce3d1780633ffc7337fa6f3da4fc
JericoRevote <JericoRevote@02397290-7de5-4d49-84a4-86ba14fd681f>
2012-02-22 23:10:20 +0000

    Adding LidarViewer dataset launch scripts.
    git-svn-id: https://www.underworldproject.org/svn/LidarViewer@8 02397290-7de5-4d49-84a4-86ba14fd681f

--- Scripts/launchElMayor-1.0-068.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/packages/LidarViewer/2.5.1/bin/LidarViewer points.out -rootSection dante
---
+++ Scripts/launchWatershedSciencesBridgeCreek-2.2-003.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/local/packages/LidarViewer/2.8/bin/LidarViewer points.out -rootSection dante
